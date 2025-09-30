b332a3d58af9bfaa4ab3282cfacad090733236ca
1enn0 <9619274+1enn0@users.noreply.github.com>
2025-09-30 19:11:47 +0200

    add stylua.toml and apply formatting

--- lua/persistent-breakpoints/config.lua
+++ lua/persistent-breakpoints/config.lua
@@ -1,12 +1,11 @@
 local config = {}
 
 config = {
-	save_dir = vim.fn.stdpath('data') .. '/nvim_checkpoints',
-	load_breakpoints_event = nil,
-	perf_record = false,
-	on_load_breakpoint = nil,
-	always_reload = false,
+  save_dir = vim.fn.stdpath("data") .. "/nvim_checkpoints",
+  load_breakpoints_event = nil,
+  perf_record = false,
+  on_load_breakpoint = nil,
+  always_reload = false,
 }
 
 return config
-
